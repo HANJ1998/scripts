89940a36fba954c591b9ca4e7b8bc49267645bad
HANJ1998 <hanj1998@foxmail.com>
2026-04-09 23:48:57 +0800

    Update 提取集思录.js

--- 提取集思录.js
+++ 提取集思录.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         提取集思录
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  获取网页上的表格内容，并在新页面展示
 // @author       hanj1998@foxmail.com
 // @match        https://www.jisilu.cn/*
