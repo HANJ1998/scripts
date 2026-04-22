3d392216c1e95a6fef05a77502167777a05bc122
HANJ_DESK <hanj1998@foxmail.com>
2026-04-22 21:49:47 +0800

    添加wps表格jsa环境

--- WPS_jsa.js
+++
@@ -1,7 +0,0 @@
-function 新建并保存工作表() {
-  for (let name of Range("A1:A10").Value()) {
-    Workbook.Add();
-    ActiveWorkbook.SaveAs("D:\\桌面\\test\\" + name + ".xlsx");
-    ActiveWorkbook.Close();
-  }
-}
